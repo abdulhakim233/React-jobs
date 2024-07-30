ad67e5f8015240d337bae81ef3a650f980f9f3a3
ABDULHAKIM SEID <131987988+abdulhakim233@users.noreply.github.com>
2024-07-30 22:35:24 +0300

    added link component

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,14 @@ import {
 
 import HomePage from './pages/HomePage';
 import MainLayout from './layouts/MainLayout';
+import JobsPage from './pages/JobsPage';
 
 const router = createBrowserRouter(
  createRoutesFromElements(
-  <Route path= '/' element={<MainLayout/>}>
+   <Route path= '/' element={<MainLayout/>}>
      <Route index element={<HomePage/>}/>
-    </Route> 
+     <Route path='/jobs' element={<JobsPage/>}/>
+   </Route> 
  )
 );
 
