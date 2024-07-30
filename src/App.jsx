c5f1e98b6ba22f381ab4b9faec3e1e3438b32a7f
ABDULHAKIM SEID <131987988+abdulhakim233@users.noreply.github.com>
2024-07-30 14:48:57 +0300

    ViewAllJobs Button added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import  Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import HomeCards from './components/HomeCards.jsx'
 import JobListings from "./components/JobListings.jsx";
+import ViewAllJobs from "./components/ViewAllJobs.jsx";
 const App = () => {
   return (
     <>
@@ -9,17 +10,11 @@ const App = () => {
     <Hero/>
     <HomeCards/>
     <JobListings/>
-
+    <ViewAllJobs/>
     
     
 
-    <section className="m-auto max-w-lg my-10 px-6">
-      <a
-        href="jobs.html"
-        className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-        >View All Jobs</a
-      >
-    </section>
+   
     </>
   )
 }
