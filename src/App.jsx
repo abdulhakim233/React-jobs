a5eddd18523a238fb8145fdb457a3661cf71e51b
ABDULHAKIM SEID <131987988+abdulhakim233@users.noreply.github.com>
2024-07-31 11:49:02 +0300

    Spinner added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import {
-   Route,
-   createBrowserRouter,
-   createRoutesFromElements ,
-   RouterProvider,
-  } from 'react-router-dom';
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements ,
+  RouterProvider,
+ } from 'react-router-dom';
 
 import HomePage from './pages/HomePage';
 import MainLayout from './layouts/MainLayout';
@@ -11,18 +11,18 @@ import JobsPage from './pages/JobsPage';
 import NotFoundPage from './pages/NotFoundPage';
 
 const router = createBrowserRouter(
- createRoutesFromElements(
-   <Route path= '/' element={<MainLayout/>}>
-     <Route index element={<HomePage/>}/>
-     <Route path='/jobs' element={<JobsPage/>}/>
-     <Route path='*' element={< NotFoundPage/>}/>
-   </Route> 
- )
+createRoutesFromElements(
+  <Route path= '/' element={<MainLayout/>}>
+    <Route index element={<HomePage/>}/>
+    <Route path='/jobs' element={<JobsPage/>}/>
+    <Route path='*' element={< NotFoundPage/>}/>
+  </Route> 
+)
 );
 
 const App = () =>{
 
-  return <RouterProvider  router ={router}/>
+ return <RouterProvider  router ={router}/>
 
 };
 
